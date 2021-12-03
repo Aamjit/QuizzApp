d4e6a3f713c5503c12d0f0a5e0d9892b30697d1d
aamjit <aayanglem@gmail.com>
2021-12-03 15:19:05 +0530

    -A lot of stuff.

--- app/src/main/java/com/example/quizapp/QuestionsGeography.java
+++ app/src/main/java/com/example/quizapp/QuestionsGeography.java
@@ -1,8 +1,10 @@
 package com.example.quizapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -128,6 +130,7 @@ public class QuestionsGeography extends AppCompatActivity {
         });
 
         quitButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),ResultActivity.class);
